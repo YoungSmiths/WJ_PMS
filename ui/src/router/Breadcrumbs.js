0178f4d5f92aaa18e66a4df8d48d574bfbf93072
YoungSmiths <young_smith@163.com>
2018-08-25 10:05:26 +0800

    delete

--- ui/src/router/Breadcrumbs.js
+++
@@ -1,9 +0,0 @@
-export default {
-    template: '' +
-    '<nav class="breadcrumb" v-if="$breadcrumbs.length">' +
-    '    <router-link class="breadcrumb-item" v-for="(crumb, key) in $breadcrumbs" :to="linkProp(crumb)" :key="key">' +
-    '        {{ crumb | crumbText }}' +
-    '    </router-link>' +
-    '</nav>'
-}
-
